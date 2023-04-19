fc7c5adf615c5381fbe0e3d5035a63dcc2b6d6a5
Gyuracz <gyuraczoliver@gmail.com>
2023-04-19 20:17:58 +0200

    2. feladat
    - service-k készen vannak
    - instructor teljesen kész
    - komponensek maradtak

--- src/app/semester/semester.service.ts
+++ src/app/semester/semester.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { RequestService } from '../request.service';
+import { Observable } from 'rxjs'
+import { Semester } from './semester.model';
+
+const URL = "api/semesters";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SemesterService {
 
-  constructor() { }
+  constructor(private requestService: RequestService) { }
+
+  getSemesters(): Observable<any>{
+    return this.requestService.get<Array<Semester>>(URL);
+  }
+
+  getSemesterById(id: number): Observable<any>{
+    return this.requestService.get(`${URL}/${id}`);
+  }
+
+  createSemester(semester: Semester): Observable<any>{
+    return this.requestService.post(`${URL}/`, semester);
+  }
+
+  updateSemester(semester: Semester): Observable<any>{
+    return this.requestService.put(`${URL}/`, semester);
+  }
+
+  deleteSemesterById(id: number): Observable<any>{
+    return this.requestService.delete(`${URL}/${id}`);
+  }
+
 }
--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { RequestService } from '../request.service';
+import { Observable } from 'rxjs'
+import { Student } from './student.model';
+
+const URL = "api/students";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(private requestService: RequestService) { }
+
+  getStudents(): Observable<any>{
+    return this.requestService.get<Array<Student>>(URL);
+  }
+
+  getStudentById(id: number): Observable<any>{
+    return this.requestService.get(`${URL}/${id}`);
+  }
+
+  createStudent(student: Student): Observable<any>{
+    return this.requestService.post(`${URL}/`, student);
+  }
+
+  updateStudent(student: Student): Observable<any>{
+    return this.requestService.put(`${URL}/`, student);
+  }
+
+  deleteStudentById(id: number): Observable<any>{
+    return this.requestService.delete(`${URL}/${id}`);
+  }
+
 }
